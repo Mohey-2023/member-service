479540ef92083526b29d357ace047c35c26e2b2f
wns882@naver.com <wns882@naver.com>
2023-08-02 23:05:09 +0900

    :zap: feat(Device) 기기토큰정보 가져오기 기능 구현
     기기토큰정보 가져오기 기능 구현 및 테스트 구현 완료

--- src/main/java/com/mohey/memberservice/repository/MemberDeviceRepository.java
+++ src/main/java/com/mohey/memberservice/repository/MemberDeviceRepository.java
@@ -1,9 +1,12 @@
 package com.mohey.memberservice.repository;
 
+import com.mohey.memberservice.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mohey.memberservice.domain.MemberDevice;
 
-public interface MemberDeviceRepository extends JpaRepository<MemberDevice, Long> {
+import java.util.List;
 
+public interface MemberDeviceRepository extends JpaRepository<MemberDevice, Long> {
+    List<MemberDevice> findMemberDevicesByMemberId(Member member);
 }
