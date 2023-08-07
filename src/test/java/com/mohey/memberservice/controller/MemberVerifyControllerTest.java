a95cdbbf271129e2a0e2a7a2d97cf10b5d3eaad9
wns882@naver.com <wns882@naver.com>
2023-08-08 00:48:07 +0900

    :art: refactor(Member) JWT검증 필요한 api Controller 이동
    JWT검증 필요한 api Controller 이동 : 내 정보 수정, 내 정보 보기

---
+++ src/test/java/com/mohey/memberservice/controller/MemberVerifyControllerTest.java
@@ -0,0 +1,151 @@
+package com.mohey.memberservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mohey.memberservice.domain.Member;
+import com.mohey.memberservice.dto.memberjoin.JoinReqDto;
+import com.mohey.memberservice.dto.memberupdate.UpdateInfoReqDto;
+import com.mohey.memberservice.repository.MemberInfoRepository;
+import com.mohey.memberservice.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class MemberVerifyControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberInfoRepository memberInfoRepository;
+    static final String memberUuid = UUID.randomUUID().toString();
+
+    @BeforeEach
+    public void setUp() {
+        //공통 given
+        JoinReqDto joinReqDto = new JoinReqDto();
+        joinReqDto.setBirthDate("1994-12-01");
+        joinReqDto.setMemberUuid(memberUuid);
+        joinReqDto.setGender("MAN");
+        Member member = memberRepository.save(joinReqDto.toMemberEntity());
+
+        joinReqDto.setNickname("qqq");
+        joinReqDto.setProfile_url("aaaaa");
+        joinReqDto.setSelfIntroduction("hahahahahaha");
+        memberInfoRepository.save(joinReqDto.toMemberInfoEntity(member));
+
+
+    }
+
+    @Transactional
+    @Test
+    public void getInfo_success_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/members/verify/{memberUuid}/getInfo", memberUuid)
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody =
+                resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Transactional
+    @Test
+    public void getInfo_fail_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/members/verify/{memberUuid}/getInfo", "2532")
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+        String responseBody =
+                resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Transactional
+    @Test
+    public void infoUpdate_success_test() throws Exception {
+        // given
+
+        List<String> interests = new ArrayList<>();
+        interests.add("운동");
+        interests.add("기타");
+        interests.add("학습");
+        UpdateInfoReqDto updateInfoReqDto = new UpdateInfoReqDto();
+        updateInfoReqDto.setSelfIntroduction("gdsgs");
+        //	updateInfoReqDto.setInterests(interests);
+        //updateReqDto.setNickname("바꿔바꿔");
+        updateInfoReqDto.setMemberUuid(memberUuid);
+        //updateReqDto.setProfile_url("www.어쩌고자짜거");
+
+        String requestBody = om.writeValueAsString(updateInfoReqDto);
+        System.out.println("테스트 : " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(post("/members/verify/updateInfo").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody =
+                resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isCreated());
+    }
+
+    @Transactional
+    @Test
+    public void infoUpdate_fail_test() throws Exception {
+        // given
+        List<String> interests = new ArrayList<>();
+        interests.add("운동");
+        interests.add("기타");
+        interests.add("학습");
+        UpdateInfoReqDto updateInfoReqDto = new UpdateInfoReqDto();
+        updateInfoReqDto.setSelfIntroduction("gdsgs");
+        updateInfoReqDto.setInterests(interests);
+        //updateReqDto.setNickname("바꿔바꿔");
+        //updateReqDto.setMemberUuid(memberUuid);
+        //updateReqDto.setProfile_url("www.어쩌고자짜거");
+
+        String requestBody = om.writeValueAsString(updateInfoReqDto);
+        System.out.println("테스트 : " + requestBody);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(post("/members/verify/updateInfo").content(requestBody).contentType(MediaType.APPLICATION_JSON));
+        String responseBody =
+                resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isBadRequest());
+    }
+}
