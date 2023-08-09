4da068878826a92dde088c0877483259e2cf1987
박현철 <wns882@naver.com>
2023-08-09 16:19:26 +0900

    :zap: feat(Search) 검색 Like로 전환
    검색 Like로 전환

--- src/main/java/com/mohey/memberservice/service/MemberSearchServiceImpl.java
+++ src/main/java/com/mohey/memberservice/service/MemberSearchServiceImpl.java
@@ -1,56 +1,59 @@
 package com.mohey.memberservice.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
 import com.mohey.memberservice.domain.Member;
 import com.mohey.memberservice.dto.memberSearch.MemberSearchRespDto;
 import com.mohey.memberservice.ex.CustomApiException;
 import com.mohey.memberservice.repository.MemberRepository;
 import com.mohey.memberservice.repository.MemberSearchRepository;
-import org.springframework.stereotype.Service;
-import lombok.RequiredArgsConstructor;
 
-import javax.transaction.Transactional;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
-public class MemberSearchServiceImpl implements MemberSearchService{
+public class MemberSearchServiceImpl implements MemberSearchService {
 
-    private final MemberSearchRepository memberSearchRepository;
-    private final MemberRepository memberRepository;
+	private final MemberSearchRepository memberSearchRepository;
+	private final MemberRepository memberRepository;
 
-    @Transactional
-    @Override
-    public List<MemberSearchRespDto> Search(String nickname, String memberUuId) {
+	@Transactional
+	@Override
+	public List<MemberSearchRespDto> Search(String nickname, String memberUuId) {
 
-        List<MemberSearchRespDto> memberList;
-        try {
+		List<MemberSearchRespDto> memberList;
+		try {
 
-            //닉네임으로 멤버들 찾아오기
-            Member me = memberRepository.findByMemberUuid(memberUuId);
-            //System.out.println(nickname);
-            List<Member> memberIdList = memberSearchRepository.findAllByNickname(nickname);
-            //System.out.println("getMy");
-            //System.out.println(my);
+			//닉네임으로 멤버들 찾아오기
+			Member me = memberRepository.findByMemberUuid(memberUuId);
+			//System.out.println(nickname);
+			List<Member> memberIdList = memberSearchRepository.findAllByNickname(nickname + "%");
+			//System.out.println("getMy");
+			//System.out.println(my);
 
-            //1. 찾은 멤버들로 필요한 정보 불러오기
-            memberList = memberSearchRepository.findMemberList(memberIdList.get(0).getId(), me);
-            //System.out.println("mmList1");
+			//1. 찾은 멤버들로 필요한 정보 불러오기
+			memberList = memberSearchRepository.findMemberList(memberIdList.get(0).getId(), me);
+			//System.out.println("mmList1");
 
-            for (int i = 0; i < memberIdList.size() - 1; i++) {
-                memberList.addAll(memberSearchRepository.findMemberList(memberIdList.get(i).getId(),me));
-                //System.out.println("mmList2");
-            }
-            //System.out.println(memberList.toString());
-            //친구가 아닌 사용자면 FriendStatus가 null로 떠버린다...이거 괜찮은가?
+			for (int i = 0; i < memberIdList.size() - 1; i++) {
+				memberList.addAll(memberSearchRepository.findMemberList(memberIdList.get(i).getId(), me));
+				//System.out.println("mmList2");
+			}
+			//System.out.println(memberList.toString());
+			//친구가 아닌 사용자면 FriendStatus가 null로 떠버린다...이거 괜찮은가?
 
-        } catch (IndexOutOfBoundsException e) {
-            throw new CustomApiException("목록에 표시할 사용자가 없습니다.");
-        } catch (Exception e) {
-            //System.out.println(e.getMessage());
-            throw new CustomApiException("사용자목록 가져오기 실패");
-        }
-        return memberList;
-    }
+		} catch (IndexOutOfBoundsException e) {
+			throw new CustomApiException("목록에 표시할 사용자가 없습니다.");
+		} catch (Exception e) {
+			//System.out.println(e.getMessage());
+			throw new CustomApiException("사용자목록 가져오기 실패");
+		}
+		return memberList;
+	}
 }
 
 
