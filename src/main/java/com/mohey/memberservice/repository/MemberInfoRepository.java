1da68e9a6ee727d8b96fdb83b9fa427b29463d74
박현철 <wns882@naver.com>
2023-08-14 10:40:43 +0900

    :zap: feat(Info) UserNickname 중복 체크
    UserNickname 중복 체크

--- src/main/java/com/mohey/memberservice/repository/MemberInfoRepository.java
+++ src/main/java/com/mohey/memberservice/repository/MemberInfoRepository.java
@@ -1,6 +1,5 @@
 package com.mohey.memberservice.repository;
 
-import com.mohey.memberservice.dto.memberInfo.GetYourInfoRespDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import com.mohey.memberservice.domain.Member;
 import com.mohey.memberservice.domain.MemberInfo;
 import com.mohey.memberservice.dto.memberInfo.GetInfoRespDto;
+import com.mohey.memberservice.dto.memberInfo.GetYourInfoRespDto;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfo, Long> {
 	@Query("SELECT new com.mohey.memberservice.dto.memberInfo.GetInfoRespDto(" +
@@ -26,27 +26,30 @@ public interface MemberInfoRepository extends JpaRepository<MemberInfo, Long> {
 	GetInfoRespDto getUserInfo(@Param("id") Long id);
 
 	@Query("SELECT new com.mohey.memberservice.dto.memberInfo.GetYourInfoRespDto(" +
-			"MI.nickname, " +
-			"M.gender, " +
-			"M.birthDate, " +
-			"MPI.profileUrl, " +
-			"MI.selfIntroduction, " +
-			"(SELECT COUNT(FR) FROM FriendRelation FR WHERE FR.memberId = M AND FR.friendStatus = true), " +
-			"FRR.friendStatus, " +
-			"FRR.favoriteStatus, " +
-			"FRS.status) " +
-			"FROM Member M " +
-			"LEFT JOIN MemberInfo MI ON MI.memberId = M AND MI.createdDatetime = (SELECT MAX(mi.createdDatetime) FROM MemberInfo mi WHERE mi.memberId = M) "
-			+
-			"LEFT JOIN MemberProfileImage MPI ON MPI.memberId = M AND MPI.createdDatetime = (SELECT MAX(mpi.createdDatetime) FROM MemberProfileImage mpi WHERE mpi.memberId = M) "
-			+
-			"LEFT JOIN FriendRelation FRR ON FRR.memberId = :member AND FRR.friendId = :friend "
-			+
-			"LEFT JOIN FriendRequest FRQ ON FRQ.memberId = :member AND FRQ.responseId = :friend AND FRQ.createdDatetime = (SELECT MAX(frq.createdDatetime) FROM FriendRequest frq WHERE frq.memberId = :member AND frq.responseId = :friend)" +
-			"LEFT JOIN FriendRequestStatus FRS ON FRS.id = FRQ.id " +
-			"WHERE M = :friend")
+		"MI.nickname, " +
+		"M.gender, " +
+		"M.birthDate, " +
+		"MPI.profileUrl, " +
+		"MI.selfIntroduction, " +
+		"(SELECT COUNT(FR) FROM FriendRelation FR WHERE FR.memberId = M AND FR.friendStatus = true), " +
+		"FRR.friendStatus, " +
+		"FRR.favoriteStatus, " +
+		"FRS.status) " +
+		"FROM Member M " +
+		"LEFT JOIN MemberInfo MI ON MI.memberId = M AND MI.createdDatetime = (SELECT MAX(mi.createdDatetime) FROM MemberInfo mi WHERE mi.memberId = M) "
+		+
+		"LEFT JOIN MemberProfileImage MPI ON MPI.memberId = M AND MPI.createdDatetime = (SELECT MAX(mpi.createdDatetime) FROM MemberProfileImage mpi WHERE mpi.memberId = M) "
+		+
+		"LEFT JOIN FriendRelation FRR ON FRR.memberId = :member AND FRR.friendId = :friend "
+		+
+		"LEFT JOIN FriendRequest FRQ ON FRQ.memberId = :member AND FRQ.responseId = :friend AND FRQ.createdDatetime = (SELECT MAX(frq.createdDatetime) FROM FriendRequest frq WHERE frq.memberId = :member AND frq.responseId = :friend)"
+		+
+		"LEFT JOIN FriendRequestStatus FRS ON FRS.id = FRQ.id " +
+		"WHERE M = :friend")
 	GetYourInfoRespDto getYourInfo(@Param("member") Member member, @Param("friend") Member friend);
 
 	MemberInfo findMemberInfoByMemberId(Member member);
+
+	Boolean existsByNickname(String name);
 
 }
