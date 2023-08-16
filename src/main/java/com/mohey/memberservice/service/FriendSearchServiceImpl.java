164f2874cdb1f2c40cb2a629d1d5195f868b8c48
Soobin-Park <jrkd2165@gmail.com>
2023-08-16 21:12:05 +0900

    :bug: feat(memberService) 친구 검색시 중복하여 뜨는 현상 수정

--- src/main/java/com/mohey/memberservice/service/FriendSearchServiceImpl.java
+++ src/main/java/com/mohey/memberservice/service/FriendSearchServiceImpl.java
@@ -38,19 +38,23 @@ public class FriendSearchServiceImpl implements FriendSearchService {
 			//            if (friendIdList.isEmpty()) {
 			//                throw new CustomApiException("목록에 표시할 친구가 없습니다.");
 			//            }
-
+			//System.out.println(friendIdList.size());
+			//System.out.println(friendIdList.get(0).getId());
 			///!!!!!!여기서 추가해줬으면 리스트에서 빼자 !!!!!!!!
+
 			//친한 친구 불러오기 << 여기서 favorite_status = null이 추가된다 왜지?? 계속 나에 대한 정보를 불러왔어서 그런듯
 			friendList = friendSearchRepository.findFavoriteFriendList(friendIdList.get(0).getId(),my);
+			if (!friendList.isEmpty() && friendList.get(0).getMemberUuid().equals(friendIdList.get(0).getMemberUuid()))
+				friendIdList.remove(0);
 
-			System.out.println(friendIdList.size());
+			//System.out.println(friendIdList.size());
 			for (int i = 1; i < friendIdList.size(); i++) {
 				friendList.addAll(friendSearchRepository.findFavoriteFriendList(friendIdList.get(i).getId(),my));
 				//들어갔으면
 				if (friendList.get(friendList.size()-1).getMemberUuid().equals(friendIdList.get(i).getMemberUuid()))
 					friendIdList.remove(i);
 			}
-			System.out.println(friendIdList.size());
+			//System.out.println(friendIdList.size());
 
 			//그냥 친구 불러오기
 			//친한 친구...인걸 여기서도 불러오는구나
